054c2a67eb00e5c53bbf2f821a4ffcad9518d80e
Gláucia Isidoro <glaucia.isidoro@gmail.com>
2019-03-17 09:37:47 -0300

    GraphiQL for requests

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -25,7 +25,7 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } }
+      args: { id: { type: GraphQLString } },
       resolve(parent, args){
         //code to get data from db/other source
         return _.find(books, {id: args.id });
