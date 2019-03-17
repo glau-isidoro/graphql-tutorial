e015ad1a8fcacd4080c1ed4444cdcd2b3e53097c
Gláucia Isidoro <glaucia.isidoro@gmail.com>
2019-03-17 13:53:08 -0300

    connection to mongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connect to mongoDB atlas
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@graphql-tutorial-6xhyi.mongodb.net/test?retryWrites=true`, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('connected do mongoDB')
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
